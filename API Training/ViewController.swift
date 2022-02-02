24cd6dcb245d9940cfaf39b868b4244427f49d2f
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2022-02-02 17:42:44 +0200

    Added URLSession

--- API Training/ViewController.swift
+++ API Training/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Post
+    struct Post: Codable {
+        let userID: Int
+        let id: Int
+        let title: String
+        let body: String
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        enum CodingKeys: String, CodingKey {
+            case userID = "userId"
+            case id = "id"
+            case title = "title"
+            case body = "body"
+        }
     }
 
-
+    
+    @IBAction func getTapped(_ sender: UIButton) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let response = response {
+                print(response)
+            }
+            
+            if let error = error {
+                print(error)
+            }
+            do {
+                guard let data = data else { return }
+                let json = try JSONDecoder().decode([Post].self, from: data)
+                print(json)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
+    
+    @IBAction func postTapped(_ sender: UIButton) {
+        
+    }
 }
 
