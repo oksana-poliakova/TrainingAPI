9bf0aa85986b293fc63838a854d6cdaaef24604b
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2022-02-07 15:53:26 +0200

    Added post requests

--- API Training/ViewController.swift
+++ API Training/ViewController.swift
@@ -10,11 +10,11 @@ import UIKit
 class ViewController: UIViewController {
     
     // MARK: - Post
-    struct Post: Codable {
-        let userID: Int
-        let id: Int
-        let title: String
-        let body: String
+    struct GetInfo: Codable {
+        let userID: Int?
+        let id: Int?
+        let title: String?
+        let body: String?
 
         enum CodingKeys: String, CodingKey {
             case userID = "userId"
@@ -23,6 +23,18 @@ class ViewController: UIViewController {
             case body = "body"
         }
     }
+    
+    struct Post: Codable {
+        let username: String?
+        let age: String?
+        
+        enum CodingKeys: String, CodingKey {
+            case username = "username"
+            case age = "age"
+        }
+    }
+    
+    
 
     
     @IBAction func getTapped(_ sender: UIButton) {
@@ -37,7 +49,7 @@ class ViewController: UIViewController {
             }
             do {
                 guard let data = data else { return }
-                let json = try JSONDecoder().decode([Post].self, from: data)
+                let json = try JSONDecoder().decode([GetInfo].self, from: data)
                 print(json)
             } catch {
                 print(error.localizedDescription)
@@ -46,6 +58,34 @@ class ViewController: UIViewController {
     }
     
     @IBAction func postTapped(_ sender: UIButton) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        var request = URLRequest(url: url)
+        let parameters: [String: Any] = [
+            "username": "Masha",
+            "age": "7"
+        ]
+        request.httpMethod = "POST"
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else { return }
+        request.httpBody = httpBody
+        request.setValue("Application/json", forHTTPHeaderField: "Content-Type")
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let response = response {
+                print(response)
+            }
+            
+            if let error = error {
+                print(error)
+            }
+            
+            do {
+                guard let data = data else { return }
+                let json = try JSONDecoder().decode(Post.self, from: data)
+                print(json)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }.resume()
         
     }
 }
